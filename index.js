1e8b993cb6dc4225f4e2366e39a018f47614dd1a
Mkscode92 <142264154+Mkscode92@users.noreply.github.com>
2024-07-11 22:17:09 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("#hls").click(function() {
+        $("#player").attr("src", "https://mkscode92.github.io/AiQworkflow1-/HLS.html");
+    });
+    $("#yt").click(function() {
+        $("#player").attr("src", "https://mkscode92.github.io/AiQworkflow1-/youtube/YouTube.html");
+    });
+    $("#dolby").click(function() {
+        $("#player").attr("src", "https://tvipnet1.github.io/AiQworkflow/");
+    });
+});
